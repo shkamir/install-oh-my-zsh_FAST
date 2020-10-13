f4a565c7d85098d5aa234b83cd7ca364acb4efdf
amirhossein <kingoflongnight@gmail.com>
2020-10-13 12:38:15 +0330

    Initial commit.

---
+++ if-on-debian-based/curl/fast-install-oh-my-zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
---
+++ if-on-redhat-based/curl/fast-install-oh-my-zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo yum install zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
